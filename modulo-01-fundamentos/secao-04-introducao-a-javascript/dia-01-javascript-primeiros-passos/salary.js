0471d014d51ecff510e011e0d90299612b066fa6
LiukenMonteiro <liuken37@gmail.com>
2022-09-26 23:19:44 -0300

    salary

---
+++ modulo-01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/salary.js
@@ -0,0 +1,18 @@
+let movie = 'Avengers'; //string literal
+let score = 10; //nummber literal
+let isValid = true; //boolean
+let name; //undefined
+let color = null; //redefinição
+
+let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3);
+
+salary++;
+salary--;
+
+console.log(salary);
