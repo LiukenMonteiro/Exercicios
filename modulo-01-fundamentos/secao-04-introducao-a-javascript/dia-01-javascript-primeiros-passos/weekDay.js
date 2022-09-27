393ff58bcb47f4b6ae3790135dc04de23985f520
LiukenMonteiro <liuken37@gmail.com>
2022-09-27 00:00:13 -0300

    sabado

---
+++ modulo-01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/weekDay.js
@@ -0,0 +1,12 @@
+let weekDay = 'sabado';
+
+if(weekDay === 'segunda-feira' || weekDay ===  'terça-feira' || weekDay === 'quarta-feira' || 
+weekDay === 'quinta-feira' || weekDay === 'sexta-feira'){
+    console.log('Oba, mais um dia de apendizado na Trybe >:D');
+}else if(weekDay === 'sabado' || weekDay ===  'domingo'){
+    console.log('FINALMENTE, descanso merecido UwU');
+}else{
+    console.log('Entrada inválida!');
+}
+
+
