ca53d614a78edab4de63aec562c7815c23a9b992
LiukenMonteiro <liuken37@gmail.com>
2022-09-27 00:11:33 -0300

    swithc case

---
+++ modulo-01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/switch-case.js
@@ -0,0 +1,19 @@
+let nota = 'aprovada';
+
+switch(nota){
+    case 'aprovada':
+        console.log('Parabéns, você foi aprovada(o)!');
+        break;
+
+    case 'lista':
+        console.log('Você está na nossa lista de espera');
+        break;
+
+    case 'reprovada':  
+    console.log('Você foi reprovada(o)');
+        break; 
+
+    default:
+    console.log('Informação incorreta');
+    break; 
+}
