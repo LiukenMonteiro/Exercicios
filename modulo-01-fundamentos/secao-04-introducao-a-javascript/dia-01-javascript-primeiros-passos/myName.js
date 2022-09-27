5b02bfa5ac0cf8939b74a3aab00dd5103a9ea0c5
LiukenMonteiro <liuken37@gmail.com>
2022-09-26 23:12:54 -0300

    váiaveis

---
+++ modulo-01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/myName.js
@@ -0,0 +1,12 @@
+const myName = 'Liuken';
+const birthCity = 'Presidente Dutra - Ma';
+
+let birthYear = 1999;
+
+birthYear = 2030;
+birthCity = 'Brasília';
+
+console.log(myName); 
+console.log(birthCity);
+console.log(birthYear);
+
