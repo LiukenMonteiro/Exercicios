7f3dc90f10f0bb15c673997bc9b408996018376a
LiukenMonteiro <liuken37@gmail.com>
2022-09-27 12:43:51 -0300

    olá

---
+++ modulo-01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/ola.js
@@ -0,0 +1,9 @@
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+console.log('Olá',names.person1);
+console.log('Olá',names.person2);
+console.log('Olá',names.person3);
