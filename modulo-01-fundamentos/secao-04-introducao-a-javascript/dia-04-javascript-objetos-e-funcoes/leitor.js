febe21adf4776e8bc6c7b6bb6c76de30d60df1bf
LiukenMonteiro <liuken37@gmail.com>
2022-09-27 12:55:40 -0300

    objeos e for of emm js

---
+++ modulo-01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/leitor.js
@@ -0,0 +1,32 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  leitor.livrosFavoritos.push(
+    {
+      titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+      autor: 'JK Rowling',
+      editor: 'Rocco',
+    },
+  );
+  // Duas formas de acessar as chaves dos objetos
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
+
+// ou
+// Duas formas de acessar a chave "livrosFavoritos"
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.');
+
+// ou
+
+console.log(leitor['nome'] + ' tem ' + leitor['livrosFavoritos'].length + ' livros favoritos.');
+
+console.log('O livro favorito de ' + leitor['nome'] + ' ' + leitor['sobrenome'] + ' se chama "' + leitor['livrosFavoritos'][0]['titulo'] + '"');
