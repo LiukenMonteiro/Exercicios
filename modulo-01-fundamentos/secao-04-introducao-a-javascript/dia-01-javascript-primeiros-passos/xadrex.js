79c032d59f91b69569ed422472d0f434b15165da
LiukenMonteiro <liuken37@gmail.com>
2022-09-27 00:44:42 -0300

    xadrez

---
+++ modulo-01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/xadrex.js
@@ -0,0 +1,24 @@
+const peao = 'frente';
+
+switch(peao.toLowerCase()) {
+
+    case 'diagonal':
+      console.log('Movimento errado');
+      break;
+
+    case 'esquerda':
+      console.log('Movimento errado');
+      break;
+
+    case 'direita':
+      console.log('Movimento errado');
+      break;
+
+    case 'frente':
+      console.log('Está certo!!!');
+      break;
+
+    default:
+      console.log('Esse movimento não existe');
+      
+  }
