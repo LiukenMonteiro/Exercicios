3fb57847273fdcb2486f51067d92927c55007e9a
LiukenMonteiro <liuken37@gmail.com>
2022-09-27 13:08:44 -0300

    moradores dos blocos

---
+++ modulo-01-fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/moradores.js
@@ -0,0 +1,48 @@
+let moradores = {
+    blocoUm: [
+      {
+        nome: 'Luiza',
+        sobrenome: 'Guimarães',
+        andar: 10,
+        apartamento: 1005,
+      },
+      {
+        nome: 'William',
+        sobrenome: 'Albuquerque',
+        andar: 5,
+        apartamento: 502,
+      },
+    ],
+    blocoDois: [
+      {
+        nome: 'Murilo',
+        sobrenome: 'Ferraz',
+        andar: 8,
+        apartamento: 804,
+      },
+      {
+        nome: 'Zoey',
+        sobrenome: 'Brooks',
+        andar: 1,
+        apartamento: 101,
+      },
+    ],
+  };
+  
+let moradoresBlocoDois = moradores.blocoDois;
+let ultimoMoradorBlocoDois = moradoresBlocoDois[moradoresBlocoDois.length - 1];
+
+// Duas formas de acessar as propriedades do objeto "ultimoMoradorBlocoDois"
+console.log('O morador do bloco 2 de nome ' + ultimoMoradorBlocoDois.nome + ' ' + ultimoMoradorBlocoDois.sobrenome + ' mora no ' + ultimoMoradorBlocoDois.andar + '° andar, apartamento ' + ultimoMoradorBlocoDois.apartamento);
+
+// Ou
+
+// console.log('O morador do bloco 2 de nome ' + ultimoMoradorBlocoDois['nome'] + ' ' + ultimoMoradorBlocoDois['sobrenome'] + ' mora no ' + ultimoMoradorBlocoDois['andar'] + '° andar, apartamento ' + ultimoMoradorBlocoDois['apartamento']);
+
+for (let index = 0; index < moradores.blocoUm.length; index += 1) {
+    console.log(moradores.blocoUm[index].nome + ' ' + moradores.blocoUm[index].sobrenome);
+  }
+  
+  for (let index = 0; index < moradores.blocoDois.length; index++) {
+    console.log(moradores.blocoDois[index].nome + ' ' + moradores.blocoDois[index].sobrenome);
+  }
