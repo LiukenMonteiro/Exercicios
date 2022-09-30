3de7564f555afdb9f43f3985ece5f62905e2cab6
LiukenMonteiro <liuken37@gmail.com>
2022-09-30 04:22:18 -0300

    manipulação de DOM

---
+++ modulo-01-fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia-01-javascript-DOM-e-seletores/seletores.js
@@ -0,0 +1,8 @@
+document.getElementById("header-container").style.backgroundColor="rgb(0,176,105)";
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor="rgb(255,159,132)";
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor="rgb(249,219,94)";
+document.getElementById("footer-container").style.backgroundColor="rgb(0,53,51)";
+document.getElementsByTagName("h3")[0].style.backgroundColor="rgb(165,0,243)";
+document.querySelectorAll("h3")[1].style.backgroundColor="rgb(165,0,243)";
+document.getElementsByTagName("h3")[2].style.backgroundColor="rgb(35,37,37)";
+document.querySelectorAll("h3")[3].style.backgroundColor="rgb(35,37,37)";
