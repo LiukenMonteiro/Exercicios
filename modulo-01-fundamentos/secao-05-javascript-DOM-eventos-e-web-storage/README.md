ed57b45cd27b5573859f39458c010673103f70e9
LiukenMonteiro <liuken37@gmail.com>
2022-09-30 01:55:52 -0300

    iniciando bloco 5

---
+++ modulo-01-fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/README.md
@@ -0,0 +1,3 @@
+## 5. JavaScript: DOM, Eventos e Web Storage
+
+Nesse bloco aprendi sobre JavaScript: DOM, Eventos e Web Storage.
---
+++ modulo-01-fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/script.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Bob Esponja - O filme";
+document.getElementById("second-paragraph").innerText = "Melhor filme do mundo";
+document.getElementById("subtitle").innerText = "Bob esponja 2 e 3 também";
