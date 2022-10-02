bf490bba0b76f017a816d1b136596b2b87ac73fa
LiukenMonteiro <liuken37@gmail.com>
2022-10-02 16:24:03 -0300

    interação de pai pra filho de nó em DOM

---
+++ modulo-01-fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,9 @@
+document.getElementById("elementoOndeVoceEsta").parentNode.style.backgroundColor="red";
+document.getElementById("primeiroFilhoDoFilho").innerText="Olá, bom dia!";
+document.getElementById("pai").firstElementChild;
+document.getElementById("elementoOndeVoceEsta").parentNode.firstElementChild;
+document.getElementById("elementoOndeVoceEsta").parentNode.firstChild;
+document.getElementById("elementoOndeVoceEsta").parentNode.firstElementChild.nextElementSibling.nextElementSibling;
+document.getElementById("pai").firstElementChild.nextElementSibling.nextElementSibling;
+
+
