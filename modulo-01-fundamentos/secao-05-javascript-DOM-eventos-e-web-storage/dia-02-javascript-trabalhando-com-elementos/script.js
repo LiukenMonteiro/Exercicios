eed3339df9fd14de9409fc61b6990410149fd397
LiukenMonteiro <liuken37@gmail.com>
2022-10-02 21:25:54 -0300

    dia 2 bolo 5

--- modulo-01-fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
+++ modulo-01-fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/script.js
@@ -8,4 +8,28 @@ document.getElementById("pai").firstElementChild.nextElementSibling.nextElementS
 
 
 
-document.querySelector("pai");
+const pai = document.getElementById('pai');
+const irmaoElementoOndeVoceEsta = document.createElement("section");
+irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmaoElementoOndeVoceEsta);
+
+const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+const filhoElementoOndeVoceEsta = document.createElement("section");
+filhoElementoOndeVoceEsta.id = "filhoElementoOndeVoceEsta";
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+const primeiroFilhoDoFilho = document.getElementById("primeiroFilhoDoFilho");
+const filhoPrimeiroFilhoDoFilho = document.createElement("section");
+filhoPrimeiroFilhoDoFilho.id = "filhoPrimeiroFIlhoDoFilho";
+primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);
+
+const terceiroFilho = filhoPrimeiroFilhoDoFilho
+.parentElement
+.parentElement
+.nextElementSibling;
+console.log(terceiroFilho);
+
+
+
+
+
