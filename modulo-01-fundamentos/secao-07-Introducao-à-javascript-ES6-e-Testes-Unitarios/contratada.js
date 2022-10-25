71547af0c983ec6ba5be1923a046bea52ad3c4b6
LiukenMonteiro <liuken37@gmail.com>
2022-10-24 22:12:27 -0300

    projeto de cartões

---
+++ modulo-01-fundamentos/secao-07-Introducao-à-javascript-ES6-e-Testes-Unitarios/contratada.js
@@ -0,0 +1,14 @@
+const novaPessoaContrata = () => {
+    let nomeCompleto = 'Liuken Monteiro';
+    let email = 'liuken37@gmail.com';
+}
+
+const newEmployees = () => {
+    const employees = {
+        id1: '', 
+        id2: '',
+        id3: '',
+    }
+    return employees
+};
+
