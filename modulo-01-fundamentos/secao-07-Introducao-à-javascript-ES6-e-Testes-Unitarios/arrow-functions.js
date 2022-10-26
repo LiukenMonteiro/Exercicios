c7ad0916afcad64b1e1c6588fbad67212b8e1021
LiukenMonteiro <liuken37@gmail.com>
2022-10-25 23:51:47 -0300

    arrows functions

---
+++ modulo-01-fundamentos/secao-07-Introducao-à-javascript-ES6-e-Testes-Unitarios/arrow-functions.js
@@ -0,0 +1,38 @@
+// const multiplyByTwo = number => number * 2;
+// console.log(multiplyByTwo(10));
+
+// const multiplication = (number, multi) => number * multi;
+// console.log(multiplication(8, 2));
+
+// const soma = function (num1, num2){
+//     return num1 + num2
+// }
+
+// console.log(soma(2, 2));
+
+// const soma = (num1, num2) => num1 + num2
+
+// console.log(soma(2, 2));
+
+// function contaPalavras (frase) {
+//     return frase.split(' ').length;
+// }
+
+// console.log(contaPalavras('Fala tribo, beleza?'));
+
+// const contaPalavras = frase => frase.split(' ').length;
+
+// console.log(contaPalavras('Fala tribo, beleza?'));
+
+// function objetoPessoa (nome, idade) {
+//     return {
+//         nome: nome,
+//         idade: idade
+//     }
+// }
+
+// console.log(objetoPessoa('Joaquim', 25));
+
+const objetoPessoa = (nome, idade) => ({nome: nome, idade: idade});
+
+console.log(objetoPessoa('Joaquim', 25));
