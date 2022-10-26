53c2edf6e5743f5e37724e39255da102584d71a2
LiukenMonteiro <liuken37@gmail.com>
2022-10-26 17:47:23 -0300

    alteração

---
+++ modulo-01-fundamentos/secao-07-Introducao-à-javascript-ES6-e-Testes-Unitarios/dia-02-javascript-ES6-fluxo-de-execução-e-objetos/objects.js
@@ -0,0 +1,42 @@
+const coolestTvShow = {
+    name: 'BoJack Horseman',
+    genre: 'adult animation',
+    createdBy: 'Raphael Bob-Waksberg',
+    favoriteCharacter: 'Princess Carolyn',
+    quote: 'Princess Carolyn always lands on her feet.',
+    seasons: 6,
+  };
+  
+//   for (const property in coolestTvShow) {
+//     console.log(property);
+//   }
+
+// console.log(Object.keys(coolestTvShow));
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for (const index in arrayOfSkills) {
+      console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+    }
+  };
+  
+  console.log('Estudante 1');
+  listSkills(student1);
+  
+  console.log('Estudante 2');
+  listSkills(student2);
