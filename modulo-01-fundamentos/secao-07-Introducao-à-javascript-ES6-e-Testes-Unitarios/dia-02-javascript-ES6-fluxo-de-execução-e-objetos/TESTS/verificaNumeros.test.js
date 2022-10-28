82a3b0fe78632ff74ffa61d24e2a384d63483cc6
LiukenMonteiro <liuken37@gmail.com>
2022-10-28 11:35:47 -0300

    tests

--- modulo-01-fundamentos/secao-07-Introducao-à-javascript-ES6-e-Testes-Unitarios/dia-02-javascript-ES6-fluxo-de-execução-e-objetos/TESTS/verificaNumeros.test.js
+++
@@ -1,7 +0,0 @@
-const numeros = require('./verificaNumeros');
-
-describre('Requisito 1', () => {
-    it('A função recebe [1, 2, 3, 4, 5] e retorna true', () => {
-        expect(true).toEqual(numeros([1, 2, 3, 4, 5]));
-    })
-})
---
+++ modulo-01-fundamentos/secao-07-Introducao-à-javascript-ES6-e-Testes-Unitarios/dia-03-primeiros-passos-em-jest/verificaNumeros.test.js
@@ -0,0 +1,7 @@
+const numeros = require('./verificaNumeros');
+
+describe('Requisito 1', () => {
+    it('A função recebe [1, 2, 3, 4, 5] e retorna true', () => {
+        expect(true).toEqual(numeros([1, 2, 3, 4, 5]));
+    })
+})
