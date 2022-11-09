71868de572a80ee10ab5fc7f67bb8590fdd431b0
LiukenMonteiro <liuken37@gmail.com>
2022-11-09 20:03:05 -0300

    validator

--- modulo-02-Front-End/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/validator/src/main.js
+++ modulo-02-Front-End/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/validator/src/main.js
@@ -5,15 +5,14 @@ const botao = document.querySelector('#button');
 const seletor = document.querySelector('#option');
 const textoDeSaida = document.querySelector('#answer');
 
-const UUID_VERSION = 4
-
+const UUID_VERSION = 4;
 
 botao.addEventListener('click', (event) => {
     event.preventDefault();
-});
+
 
 const campos = {
-    cpf: validator.isTaxID(campoDeTexto.ariaValueMax, 'pt-br'),
+    cpf: validator.isTaxID(campoDeTexto.value, 'pt-BR'),
     hexColor: validator.isHexColor(campoDeTexto.value),
     email: validator.isEmail(campoDeTexto.value),
     uuid: validator.isUUID(campoDeTexto.value, UUID_VERSION),
@@ -21,3 +20,4 @@ const campos = {
 };
 
 textoDeSaida.innerHTML = `A validação retornou ${campos[seletor.value]}`;
+});
