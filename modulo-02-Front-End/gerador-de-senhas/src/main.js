1509810179950a9015d02c7ac7d0064f3df26166
LiukenMonteiro <liuken37@gmail.com>
2022-11-08 22:36:07 -0300

    dia 1 frontend

--- modulo-02-Front-End/gerador-de-senhas/src/main.js
+++ modulo-02-Front-End/gerador-de-senhas/src/main.js
@@ -1,9 +1,10 @@
-import { nanoid, random } from "nanoid";
+import { nanoid } from "nanoid";
+import './style.css'
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEL = document.querySelector('h2');
 
 passwordBtnEl.addEventListener('click', () =>{
     const randomPassword = nanoid();
-    displayPasswordEL.innerHTML = randomPassword
+    displayPasswordEL.innerHTML = randomPassword;
 });
