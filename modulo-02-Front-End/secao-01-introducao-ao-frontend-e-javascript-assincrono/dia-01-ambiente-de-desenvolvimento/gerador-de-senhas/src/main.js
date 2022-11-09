33e4b1572fa41c0cc185aa41fb9bd7bf04369ceb
LiukenMonteiro <liuken37@gmail.com>
2022-11-09 12:56:34 -0300

    atividade

--- modulo-02-Front-End/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ modulo-02-Front-End/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,5 +1,7 @@
 import { nanoid } from "nanoid";
-import './style.css'
+import copy from 'clipboard-copy';
+
+import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEL = document.querySelector('h2');
@@ -8,3 +10,10 @@ passwordBtnEl.addEventListener('click', () =>{
     const randomPassword = nanoid();
     displayPasswordEL.innerHTML = randomPassword;
 });
+
+displayPasswordEL.addEventListener('click',
+(event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
+});
+
