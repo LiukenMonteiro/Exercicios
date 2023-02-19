95822359cc4c9d1395cc22ae0d1b7d6b3454b8ff
LiukenMonteiro <liuken37@gmail.com>
2023-02-18 23:17:19 -0300

    constructor

--- modulo-02-Front-End/secao-03-componentes-com-estados-eventos-e-formulários-com-React/dia-01-componentes-com-estado-e-eventos/botao/src/App.js
+++ modulo-02-Front-End/secao-03-componentes-com-estados-eventos-e-formulários-com-React/dia-01-componentes-com-estado-e-eventos/botao/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 
-function clickedButtonOne() {
-    console.log('clicked button one!');
-}
-function clickedButtonTwo() {
-  console.log('clicked button two!');
-}
-function clickedButtonThree() {
-  console.log('clicked button three!');
-}
+
 class Botoes extends Component {
+
+  constructor () {
+    super();
+    this.clickedButtonOne = this.clickedButtonOne.bind(this);
+    this.clickedButtonTwo = this.clickedButtonTwo.bind(this);
+    this.clickedButtonThree = this.clickedButtonThree.bind(this);
+  }
+
+  clickedButtonOne() {
+    console.log('clicked button one!', this);
+}
+  clickedButtonTwo() {
+  console.log('clicked button two!', this);
+}
+  clickedButtonThree() {
+  console.log('clicked button three!', this);
+}
   render() {
     return (
       <main>
-      <button onClick={clickedButtonOne}>Button One</button>
-      <button onClick={clickedButtonTwo}>Button Two</button>
-      <button onClick={clickedButtonThree}>Button Three</button>
+      <button type="button" onClick={this.clickedButtonOne}>Button One</button>
+      <button type="button" onClick={this.clickedButtonTwo}>Button Two</button>
+      <button type="button" onClick={this.clickedButtonThree}>Button Three</button>
       </main>
     );
   }
