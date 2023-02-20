decd2156188238abf66a28fd582a6cecba1ed348
LiukenMonteiro <liuken37@gmail.com>
2023-02-20 11:04:14 -0300

    estado

--- modulo-02-Front-End/secao-03-componentes-com-estados-eventos-e-formulários-com-React/dia-01-componentes-com-estado-e-eventos/button/src/App.js
+++ modulo-02-Front-End/secao-03-componentes-com-estados-eventos-e-formulários-com-React/dia-01-componentes-com-estado-e-eventos/button/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
+// Agora você vai converter a aplicação desenvolvida nos exercícios anteriores para uma que conta o número de cliques dados em cada botão!
+// Defina um estado inicial para a contagem de cada botão.
 
+// 👀 De olho na dica: lembre-se que você pode definir três chaves, uma para cada botão!
+
+// Agora, quando um botão for clicado, altere de forma assíncrona o estado deste botão de zero para um.
+
+// Por fim, incremente a contagem de um botão sempre que ele for clicado.
 class Botoes extends Component {
 
   constructor () {
@@ -8,23 +15,39 @@ class Botoes extends Component {
     this.clickedButtonOne = this.clickedButtonOne.bind(this);
     this.clickedButtonTwo = this.clickedButtonTwo.bind(this);
     this.clickedButtonThree = this.clickedButtonThree.bind(this);
+    this.state = {
+      numberOfClickOne: 0,
+      numberOfClickTwo: 0,
+      numberOfClickThree: 0,
+    }
   }
 
   clickedButtonOne() {
-    console.log('clicked button one!', this);
+    this.setState((previousStateOne, _props) => ({
+      numberOfClickOne: previousStateOne.numberOfClickOne + 1
+    }))
+    // console.log('clicked button one!', this);
 }
   clickedButtonTwo() {
-  console.log('clicked button two!', this);
+    this.setState((previousStateTwo, _props) => ({
+      numberOfClickTwo: previousStateTwo.numberOfClickTwo + 1
+    }))
+  // console.log('clicked button two!', this);
 }
   clickedButtonThree() {
-  console.log('clicked button three!', this);
+    this.setState((previousStateThree, _props) => ({
+      numberOfClickThree: previousStateThree.numberOfClickThree + 1
+    }))
+  // console.log('clicked button three!', this);
 }
   render() {
+    const { numberOfClickOne, numberOfClickTwo, numberOfClickThree } = this.state;
+    // console.log(this.state);
     return (
       <main>
-      <button type="button" onClick={this.clickedButtonOne}>Button One</button>
-      <button type="button" onClick={this.clickedButtonTwo}>Button Two</button>
-      <button type="button" onClick={this.clickedButtonThree}>Button Three</button>
+      <button type="button" onClick={this.clickedButtonOne}>{`Cliques no botão 1: ${numberOfClickOne}`}</button>
+      <button type="button" onClick={this.clickedButtonTwo}>{`Cliques no botão 2: ${numberOfClickTwo}`}</button>
+      <button type="button" onClick={this.clickedButtonThree}>{`Cliques no botão 3: ${numberOfClickThree}`}</button>
       </main>
     );
   }
