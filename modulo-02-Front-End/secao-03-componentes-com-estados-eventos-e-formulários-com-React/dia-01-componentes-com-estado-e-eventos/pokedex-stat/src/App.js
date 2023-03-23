fc1a2adb6195c53c32f7cc1c88e2c9720570ad0a
LiukenMonteiro <liuken37@gmail.com>
2023-03-22 22:52:06 -0300

    pokedex stat

---
+++ modulo-02-Front-End/secao-03-componentes-com-estados-eventos-e-formulários-com-React/dia-01-componentes-com-estado-e-eventos/pokedex-stat/src/App.js
@@ -0,0 +1,16 @@
+// ./src/App.js
+import React from 'react';
+import './App.css';
+import pokemonList from './data';
+import Pokedex from './components/Pokedex';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Pokedex </h1>
+      <Pokedex pokemonList={ pokemonList } />
+    </div>
+  );
+}
+
+export default App;
